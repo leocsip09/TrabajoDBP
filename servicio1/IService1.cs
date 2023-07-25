16d1a111eceaa64b10a3221f821b0050524e5692
krospigliosig <132178872+krospigliosig@users.noreply.github.com>
2023-07-25 13:38:53 -0500

    Add files via upload

---
+++ servicio1/IService1.cs
@@ -0,0 +1,21 @@
+﻿using base_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace servicio1
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        IList<String> getCiudades();
+
+        [OperationContract]
+
+        void EnviarService(string datos);
+    }
+}
---
+++ servicio1/Service1.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using base_datos;
+
+
+namespace servicio1
+{
+    public class Service1 : IService1
+    {
+        public IList<String> getCiudades()
+        {
+            Ciudad ciudad = new Ciudad();
+            IList<String> Ciudades = new List<String>();
+            Ciudades = ciudad.getCiudades();
+            return Ciudades;
+        }
+
+        public void EnviarService(string datos)
+        {
+            Ingresar aBaseDatos = new Ingresar();
+            aBaseDatos.IngresarBaseDatos(datos);
+        }
+    }
+}
